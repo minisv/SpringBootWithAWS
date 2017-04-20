ffe4254ba9467d69667a809fe07782ed7e5d5635
minisv <minisv@naver.com>
2017-04-20 17:09:17 +0900

    RequestMethod 적용

--- src/main/java/me/kidult/Controller/MainController.java
+++ src/main/java/me/kidult/Controller/MainController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by minisv on 2017. 3. 8..
@@ -13,13 +14,13 @@ public class MainController {
 
   private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-  @RequestMapping("/")
+  @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index() {
     logger.info("log");
     return "index";
   }
 
-  @RequestMapping(value = "/swagger")
+  @RequestMapping(value = "/swagger", method = RequestMethod.GET)
   public String swagger() {
     return "swagger-ui/index";
   }
